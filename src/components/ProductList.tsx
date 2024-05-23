84b9ab0e84847002c727f5d6f492cce9fa150cb2
Chirag-04 <95089015+Chirag-04@users.noreply.github.com>
2024-05-23 21:34:36 +0530

    completed the featured products

---
+++ src/components/ProductList.tsx
@@ -0,0 +1,132 @@
+import Image from "next/image"
+import Link from "next/link"
+
+
+const ProductList = () => {
+  return (
+    <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
+
+        <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]" >
+            <div className="relative w-full h-80">
+          <Image 
+          src="https://images.pexels.com/photos/10252315/pexels-photo-10252315.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+          fill
+          sizes="25vw"
+          className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+           <Image 
+          src="https://images.pexels.com/photos/1923109/pexels-photo-1923109.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+          fill
+          sizes="25vw"
+          className="absolute object-cover rounded-md"
+
+          />
+
+          </div>
+          <div className="flex justify-between">
+
+            <span className="font-medium">Product Name</span>
+            <span className="font-semibold">$49</span>
+          </div>
+          <div className="text-sm text-gray-500">My description</div>
+          <button className="rounded-xl ring-1 ring-chirag text-chirag py-2 px-4 hover:bg-chirag w-max hover:text-white">Add to Cart</button>
+
+        </Link>
+
+        <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]" >
+            <div className="relative w-full h-80">
+          <Image 
+          src="https://images.pexels.com/photos/10252315/pexels-photo-10252315.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+          fill
+          sizes="25vw"
+          className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+           <Image 
+          src="https://images.pexels.com/photos/1923109/pexels-photo-1923109.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+          fill
+          sizes="25vw"
+          className="absolute object-cover rounded-md"
+
+          />
+
+          </div>
+          <div className="flex justify-between">
+
+            <span className="font-medium">Product Name</span>
+            <span className="font-semibold">$49</span>
+          </div>
+          <div className="text-sm text-gray-500">My description</div>
+          <button className="rounded-xl ring-1 ring-chirag text-chirag py-2 px-4 hover:bg-chirag w-max hover:text-white">Add to Cart</button>
+
+        </Link>
+
+        <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]" >
+            <div className="relative w-full h-80">
+          <Image 
+          src="https://images.pexels.com/photos/10252315/pexels-photo-10252315.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+          fill
+          sizes="25vw"
+          className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+           <Image 
+          src="https://images.pexels.com/photos/1923109/pexels-photo-1923109.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+          fill
+          sizes="25vw"
+          className="absolute object-cover rounded-md"
+
+          />
+
+          </div>
+          <div className="flex justify-between">
+
+            <span className="font-medium">Product Name</span>
+            <span className="font-semibold">$49</span>
+          </div>
+          <div className="text-sm text-gray-500">My description</div>
+          <button className="rounded-xl ring-1 ring-chirag text-chirag py-2 px-4 hover:bg-chirag w-max hover:text-white">Add to Cart</button>
+
+        </Link>
+
+        <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]" >
+            <div className="relative w-full h-80">
+          <Image 
+          src="https://images.pexels.com/photos/10252315/pexels-photo-10252315.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+          fill
+          sizes="25vw"
+          className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+           <Image 
+          src="https://images.pexels.com/photos/1923109/pexels-photo-1923109.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt=""
+          fill
+          sizes="25vw"
+          className="absolute object-cover rounded-md"
+
+          />
+
+          </div>
+          <div className="flex justify-between">
+
+            <span className="font-medium">Product Name</span>
+            <span className="font-semibold">$49</span>
+          </div>
+          <div className="text-sm text-gray-500">My description</div>
+          <button className="rounded-xl ring-1 ring-chirag text-chirag py-2 px-4 hover:bg-chirag w-max hover:text-white">Add to Cart</button>
+
+        </Link>
+
+        
+
+
+    </div>
+  )
+}
+
+export default ProductList
