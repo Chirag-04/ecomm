1a60e98ce7efae480c8978ebd1c291aad3291820
Chirag-04 <95089015+Chirag-04@users.noreply.github.com>
2024-05-25 17:01:15 +0530

    completed app fetch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { WixClientContextProvider } from "@/context/wixContext";
 export const metadata: Metadata = {
   title: "Lama Dev E-Commerce Application",
   description: "A complete e-commerce application with Next.js and Wix",
@@ -17,9 +18,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <WixClientContextProvider>
+
         <Navbar/>
         {children}
         <Footer/>
+       </WixClientContextProvider>
         
         </body>
     </html>
